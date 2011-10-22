d9e10d72a7735a09a3c4f4ae3a8a8f3a10f7adb9
Josef Kříž <pepakriz@gmail.com>
2011-10-22 17:51:15 +0200

    added scripts for cloning vendors

---
+++ bin/clone_vendors.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+DIR="$(dirname "$(readlink -f "$0")")/.."
+VENDORS_DIR="$DIR/libs-all"
+LIBS_DIR="$DIR/libs"
+
+mkdir $VENDORS_DIR
+cd $VENDORS_DIR
+
+##
+# @param destination directory (e.g. "doctrine")
+# @param URL of the git remote (e.g. git://github.com/doctrine/doctrine2.git)
+# @param revision to point the head (e.g. origin/HEAD)
+#
+install_git()
+{
+	INSTALL_DIR=$1
+	SOURCE_URL=$2
+	REV=$3
+
+	echo
+	echo "Installing $INSTALL_DIR ..."
+
+	if [ -z $REV ]; then
+		REV=origin/master
+	fi
+
+	if [ ! -d $INSTALL_DIR ]; then
+		git clone $SOURCE_URL $INSTALL_DIR
+	fi
+
+	cd $INSTALL_DIR
+	git fetch origin
+	git reset --hard $REV
+	cd ..
+}
+
+# Nette Framework
+install_git nette git://github.com/nette/nette.git
+ln -s "$VENDORS_DIR/nette/Nette" "$LIBS_DIR/Nette"
+
+
+# Venne:CMS
+install_git venne git://github.com/Venne/Venne-CMS.git
+ln -s "$VENDORS_DIR/venne/Venne" "$LIBS_DIR/Venne"
+ln -s "$VENDORS_DIR/venne/App" "$LIBS_DIR/App"
+
+# Doctrine
+install_git doctrine git://github.com/doctrine/doctrine2.git
+cd "$VENDORS_DIR/doctrine"
+git submodule init
+git submodule update
+ln -s "$VENDORS_DIR/doctrine/lib" "$LIBS_DIR/Doctrine"
+
+
